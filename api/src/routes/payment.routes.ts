817e95961a2c32f05f2607759fe349ba024343b0
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-12-14 14:32:59 -0300

    fixed

--- api/src/routes/payment.routes.ts
+++ api/src/routes/payment.routes.ts
@@ -7,25 +7,29 @@ const router = Router();
 
 router.post("/mp", checkoutMercadoPago);
 router.post("/mp/notification", async (req: Request, res: Response) => {
-  res.json("ok");
+  try {
+    res.json("ok");
 
-  const { id } = req.body.data;
-  const infoPayment: any = await axios.get(
-    `https://api.mercadopago.com/v1/payments/${id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN_MP}`,
-      },
+    const { id } = req.body.data;
+    const infoPayment: any = await axios.get(
+      `https://api.mercadopago.com/v1/payments/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN_MP}`,
+        },
+      }
+    );
+    if (
+      infoPayment.data.status === "approved" ||
+      infoPayment.data.status === "cancel"
+    ) {
+      console.log(infoPayment);
     }
-  );
-  if (
-    infoPayment.data.status === "approved" ||
-    infoPayment.data.status === "cancel"
-  ) {    
-    console.log(infoPayment);
+    //compare id_mp with preference_id
+    res.json("ok");
+  } catch (error) {
+    console.log(error);
   }
-  //compare id_mp with preference_id
-  res.json("ok");
 });
 
 export default router;
