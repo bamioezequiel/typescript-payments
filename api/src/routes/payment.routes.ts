158f72999693a686b70c549bec6b8d0707b23c6b
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-12-14 19:08:24 -0300

    test route notifacation

--- api/src/routes/payment.routes.ts
+++ api/src/routes/payment.routes.ts
@@ -7,24 +7,7 @@ const router = Router();
 
 router.post("/mp", checkoutMercadoPago);
 router.post("/mp/notification", async (req: Request, res: Response) => {
-  try {
-    res.status(200).send("ok");
-/* 
-    const { id } = req.body.data;
-    const infoPayment: any = await axios.get(
-      `https://api.mercadopago.com/v1/payments/${id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.ACCESS_TOKEN_MP}`,
-        },
-      }
-      );
-      console.log(infoPayment);
-   
-    res.json("ok"); */
-  } catch (error) {
-    console.log(error);
-  }
+    res.send("ok");
 });
 
 export default router;
