3f5d6e2f02fff976bce233533c384659dd2ed751
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-12-16 15:26:42 -0300

    updated

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -5,7 +5,7 @@ import { checkAuth, checkRoleAuth } from "../middlewares/auth.middlewares";
 const router = Router();
 
 /* router.get('/', checkAuth, checkRoleAuth(['User']), getUser); */
-router.get("/", getUser);
+router.get("/:id", getUser);
 router.post("/changeRole", checkAuth, checkRoleAuth(["Admin"]), changeRole);
 
 export default router;
